78679c624aea34b84d1369d7fe301064ccb9ff20
Darth Vader <devacademyweb@gmail.com>
2024-11-01 22:11:43 -0500

    First

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'https://bookhub-api-latest.onrender.com/api/v1'  ,
-    paypalReturnUrl: 'http://localhost:4200/customer/cart'
+    paypalReturnUrl: 'https://bookhub-web.netlify.app/customer/cart'
   };
